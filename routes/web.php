50d6967ba5e1111e1e90a3b8ef4616114558c085
phpzhou <hjhhj>
2018-05-09 18:43:36 +0800

    设置项目访问首页为登录页面

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,12 @@
 <?php
-Route::get('/',function () {
-    return view('welcome');
-});
+
 Route::get('/index',function (){
     return view('Index.index');
 });
 //商家简单表和详情表的增删该查
 Route::resource('shops','BusinessController');
 //商家登录注销
-Route::get('/login','SessionController@login')->name('login');
+Route::get('/','SessionController@login')->name('login');
 Route::post('/login','SessionController@store');
 Route::delete('/logout','SessionController@logout')->name('logout');
 //商家菜品分类的增删改查
